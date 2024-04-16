559bb48d2dfcb2f5cac77d0ae6113efc5924da41
Orçun Çolak <orcun.colak@hazelcast.com>
2024-04-16 08:23:21 +0300

    upgrade spring

--- src/main/java/com/colak/springcachecaffeineutorial/employee/config/CacheConfig.java
+++ src/main/java/com/colak/springcachecaffeineutorial/employee/config/CacheConfig.java
@@ -3,12 +3,17 @@ package com.colak.springcachecaffeineutorial.employee.config;
 import com.github.benmanes.caffeine.cache.Caffeine;
 import org.springframework.cache.CacheManager;
 import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.cache.caffeine.CaffeineCache;
 import org.springframework.cache.caffeine.CaffeineCacheManager;
+import org.springframework.cache.support.SimpleCacheManager;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 import java.time.Duration;
+import java.time.temporal.ChronoUnit;
+import java.util.ArrayList;
 import java.util.HashSet;
+import java.util.List;
 import java.util.Set;
 
 @Configuration
@@ -25,6 +30,20 @@ public class CacheConfig {
         cacheManager.setCacheNames(someCacheNames()); // Specify cache names if needed
         return cacheManager;
     }
+
+    // Custom configuration for different caches
+    // @Bean
+    // public CacheManager cacheManager() {
+    //     List<CaffeineCache> caches = new ArrayList<>();
+    //
+    //     caches.add(buildCache("PokeCache", Duration.of(10, ChronoUnit.SECONDS)));
+    //     // add more caches here ....
+    //
+    //     SimpleCacheManager cacheManager = new SimpleCacheManager();
+    //     cacheManager.setCaches(caches);
+    //
+    //     return cacheManager;
+    // }
 
     // Expire After Access: Expires the entry after the specified duration is passed since the last read or write occurs.
     // Expire After Write: Expires the entry after the specified duration is passed since the last write occurs.
